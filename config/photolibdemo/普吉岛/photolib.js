b9876558f0ff55a584a12907fa78e52498ee8e16
haitao <haitaosoft@163.com>
2020-10-02 06:47:25 +0800

    software

---
+++ config/photolibdemo/普吉岛/photolib.js
@@ -0,0 +1,6 @@
+var p_img=0;
+imgs[p_img++]="DSCN2228.JPG";
+imgs[p_img++]="DSCN2229.JPG";
+imgs[p_img++]="DSCN2238.JPG";
+imgs[p_img++]="DSCN2261.JPG";
+imgs[p_img++]="P2050020.JPG";
---
+++ config/photolibdemo/曾城白水寨/photolib.js
@@ -0,0 +1,5 @@
+var p_img=0;
+imgs[p_img++]="DSCN0828.JPG";
+imgs[p_img++]="DSCN0831.JPG";
+imgs[p_img++]="DSCN0918.JPG";
+imgs[p_img++]="DSCN0993.JPG";
